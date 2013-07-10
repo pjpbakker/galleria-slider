e745dc57546328f21b22df67e986897985d0243a
usbpaul <usb.paul@gmail.com>
2013-07-10 23:46:24 +0200

    Removed unused dev code. Added galleria scripts.

--- gallery-slider.php
+++ gallery-slider.php
@@ -22,64 +22,21 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
 */
 
-function replace_letters($content) {
-	return $content;
-}
-
-add_filter('the_content', 'replace_letters');
-
-//[foobar]
-function foobar_func( $atts ){
-	$widget_content = "<div style='color: red;'>mijn tekst</div>";
-	return $widget_content;
-	// return plugins_url();
-	}
-
-add_shortcode( 'foobar', 'foobar_func' );
-
-function get_all_images() {
-	$args = array (
-    'post_type' => 'attachment',
-    'post_status' => 'published',
-    'posts_per_page' => 25,
-    'numberposts' => null,
-	);
-	
-	$attachments = get_posts($args);
-	
-	$post_count = count($attachments);
-	
-	$widget_content = '<div class="galleria">';
-	if ($attachments) {
-	    foreach ($attachments as $attachment) {
-		    $widget_content .= "<div class=\"post photo col3\">";
-        $url = get_attachment_link($attachment->ID);      
-        $img = wp_get_attachment_url($attachment->ID);
-        $title = get_the_title($attachment->post_parent);//extraigo titulo
-        $widget_content .= '<a href="'.$url.'"><img title="'.$title.'" src="'.$img.'"></a>';
-        $widget_content .= "</div>";
-	    }   
-	}
-  $widget_content .= "</div>";
-	return $widget_content;
-}
-
-function get_tagged_images($atts) {
+function get_tagged_images($attrs) {
 	
 	extract(shortcode_atts(
 		array(
 			'tag' => 'emtpy_tag',
-		), $atts
+		), $attrs
 	));
 	global $wpdb;
 	
 	$query ="SELECT * FROM wp_posts, wp_term_relationships, wp_terms where post_type = 'attachment' AND ID = object_id AND term_taxonomy_id = term_id AND name = '" . $tag . "'";
 	$results = $wpdb->get_results($query);
 	
-	$widget_content = '<div class="galleria">';
+	$widget_content = '<div id="galleria">';
 	if ($results) {
 	    foreach ($results as $attachment) {
 	    	setup_postdata($attachment);
@@ -96,5 +53,15 @@ function get_tagged_images($atts) {
 }
 
 add_shortcode( 'galleria', 'get_tagged_images' );
+
+function load_galleria_scripts() {
+	wp_enqueue_script(
+		'galleria-1.2.9.min',
+		plugins_url( '/js/galleria/galleria-1.2.9.min.js' , __FILE__ ),
+		array( 'jquery' )
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'load_galleria_scripts' );
 
 ?>
